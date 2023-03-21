3e7fa48c2ac99fcb8d5fc2499e8e9a14e4b5e827
Sindre Moldsvor <sindre.moldsvor@gmail.com>
2023-03-21 11:51:46 +0100

    Started on exercise 2

---
+++ 2_uart/uart.c
@@ -0,0 +1,38 @@
+#include "uart.h"
+
+typedef struct {
+    volatile uint32_t TASKS_STARTRX;
+    volatile uint32_t TASKS_STOPRX;
+    volatile uint32_t TASKS_STARTTX;
+    volatile uint32_t TASKS_STOPTX;
+    volatile uint32_t RESERVED[3];
+    volatile uint32_t TASKS_SUSPEND;
+    volatile uint32_t RESERVED1[56];
+    volatile uint32_t EVENTS_CTS;
+    volatile uint32_t EVENTS_NCTS;
+    volatile uint32_t EVENTS_RXDRDY;
+    volatile uint32_t RESERVED2[4];
+    volatile uint32_t EVENTS_TXDRDY;
+    volatile uint32_t EVENTS_ERROR;
+    volatile uint32_t RESERVED3[7];
+    volatile uint32_t EVENTS_RXTO;
+    volatile uint32_t RESERVED4[46];
+    volatile uint32_t SHORTS;
+    volatile uint32_t RESERVED5[64];
+    volatile uint32_t INTENSET;
+    volatile uint32_t INTENCLR;
+    volatile uint32_t RESERVED6[93];
+    volatile uint32_t ERRORSRC;
+    volatile uint32_t RESERVED7[31];
+    volatile uint32_t ENABLE;
+    volatile uint32_t PSEL_RTS;
+    volatile uint32_t PSEL_TXD;
+    volatile uint32_t PSEL_CTS;
+    volatile uint32_t PSEL_RXD;
+    volatile uint32_t RXD;
+    volatile uint32_t TXD;
+    volatile uint32_t BAUDRATE;
+    volatile uint32_t RESERVED8[17];
+    volatile uint32_t CONFIG;
+} NRF_UART_REG;
+
---
+++ 2_uart/uart.h
@@ -0,0 +1,5 @@
+#include <stdint.h>
+
+void uart_init();
+void uart_send(char letter);
+char uart_read();
